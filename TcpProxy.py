40e5acc0085dea0a5db73c1397d90785463c5166
BruceMacD <brucewmacdonald@gmail.com>
2017-01-28 15:59:16 -0400

    Accept terminal paramaters

--- TcpProxy.py
+++ TcpProxy.py
@@ -26,3 +26,30 @@ def server_loop(local_host, local_port, remote_host, remote_port, receive_first)
         proxy_thread = threading.Thread(target=proxy_handler,
                                         args=(client_socket, remote_host, remote_port, receive_first))
         proxy_thread.start()
+
+def main():
+
+    if len(sys.argv[1:]) != 5:
+        print("Usage: ./proxy.py [localhost] [localport] [remotehost] [remoteport] [recive_first]")
+        print("Ex: ./proxy.py 127.0.0.1 9000 10.12.123.1 9000 True")
+        sys.exit(0)
+
+    local_host = sys.argv[1]
+    local_port = int(sys.argv[2])
+
+    #target
+    remote_host = sys.srgv[3]
+    remote_port = int(sys.argv[2])
+
+    #connect proxy before sending
+    receive_first = sys.argv[5]
+
+    if "True" in receive_first:
+        recieve_first = True
+    else:
+        recieve_first =False
+
+    #spin up on listening socket
+    server_loop(local_host, local_port, remote_host, remote_port, receive_first)
+
+main()
