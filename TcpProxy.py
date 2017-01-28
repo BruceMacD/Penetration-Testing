f2fa36734a8d4488d750a27eac3ba4e78a0e48fc
BruceMacD <brucewmacdonald@gmail.com>
2017-01-28 17:02:16 -0400

    Communication with remote
    Inspect received data

--- TcpProxy.py
+++ TcpProxy.py
@@ -27,6 +27,66 @@ def server_loop(local_host, local_port, remote_host, remote_port, receive_first)
                                         args=(client_socket, remote_host, remote_port, receive_first))
         proxy_thread.start()
 
+def proxy_handler(client_socket, remote_host, remote_port, receive_first)
+
+    #connect to remote host
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect((remote_host, remote_port))
+
+    if receive_first:
+
+        remote_buffer = receive_from(remote_socket)
+        hexdump(remote_buffer)
+
+        remote_buffer = response_handler(remote_buffer)
+
+        #send data if exists
+        if len(remote_buffer):
+            print ("## Sending %d bytes to localhost." % len(remote_buffer))
+            client_socket.send(remote_buffer)
+
+        #read local, send remote loop
+        while True:
+            local_buffer = receive_from(client_socket)
+
+            if len(local_buffer):
+                print ("## Received %d bytes from localhost" % len(local_buffer))
+                hexdump(local_buffer)
+
+                #send
+                local_buffer = request_handler(local_buffer)
+                remote_socket.send(local_buffer)
+                print("## Sent to remote")
+
+            #response
+            remote_buffer = receive_from(remote_socket)
+
+            if len(remote_buffer):
+                print ("## Received %d bytes from remote" % len(remote_buffer))
+                hexdump(remote_buffer)
+
+                #send to response
+                remote_buffer = response_handler(remote_buffer)
+                client_socket.send(remote_buffer)
+                print("## Sent to localhost")
+
+            #check to close connection when no more data is being sent
+            if not len(local_buffer) or not len(remote_buffer):
+                client_socket.close()
+                remote_socket.close()
+                print("Connection closed")
+
+                break
+
+#hexdumper from http://code.activestate.com/recipes/142812-hex-dumper/
+def hexdump(src, length=16):
+
+def receive_from(connection):
+
+def request_handler(buffer):
+
+def response_handler(buffer):
+
 def main():
 
     if len(sys.argv[1:]) != 5:
