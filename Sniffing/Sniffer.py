c775885f2c8c73226b8b8b00b1d947f2a913f13d
BruceMacD <brucewmacdonald@gmail.com>
2017-02-02 22:53:03 -0400

    Decode IP header
    Decode and output IP header information

--- Sniffing/Sniffer.py
+++ Sniffing/Sniffer.py
@@ -1,8 +1,40 @@
 import socket
 import os
+import struct
+from ctypes import *
 
 # listen on
 host = "192.168.0.196"
+
+#ip header
+class IP(Structure):
+    _fields_ = [
+        ("ihl", c_ubyte, 4),
+        ("version", c_ubyte, 4),
+        ("tos", c_ubyte),
+        ("len", c_ushort),
+        ("id", c_ushort),
+        ("offset", c_ushort),
+        ("ttl", c_ubyte),
+        ("protocol_num", c_ubyte),
+        ("sum", c_ushort),
+        ("src", c_ulong),
+        ("dst", c_ulong)
+    ]
+
+    def _new_(self, socket_buffer=None):
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer=None):
+
+        # protocol names
+        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
+
+        # readable IP address
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except:
+            self.protocol = str(self.protocol_num)
 
 # create raw socket and bind it to the public interface
 if os.name == "nt":
@@ -21,8 +53,20 @@ sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 if os.name == "nt":
     sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
-# test read a single packet
-print(sniffer.recvfrom(65565))
+try:
 
-if os.name == "nt":
-    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+    while True:
+
+        #read packet
+        raw_buffer = sniffer.recvfrom(65565)[0]
+
+        # ip header = first 20 bytes
+        ip_header = IP(raw_buffer[0:20])
+
+        print("Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+
+#CTRL+C
+except KeyboardInterrupt:
+
+    if os.name == "nt":
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
